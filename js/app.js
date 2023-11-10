19b200d85ed28d25365d3d346bdc7f4e8e514548
MarK22100 <fmzambrana3@gmail.com>
2023-11-09 21:39:28 -0300

    workPrompt

--- js/app.js
+++ js/app.js
@@ -1,12 +1,51 @@
 //Inicio de Js
 //https://github.com/comision80i/Javascript.git
+/*
+let nombreUsuario
+let apellidoUsuario;
+let dniUsuario; T
 
-// TIPOS DE VARIABLES
-//
-let mensaje = "Este es un mensaje"
-const token = "token"
+console.log('El valor de nombreusuario es: --> ' + nombreUsuario);
 
-console.log(mensaje)
+console.Log('El valor de apellidoUsuario es: -->' + apellidoUsuario);
 
-//Escribir en el HMTL
-document.write(mensaje)
+console.Log('El valor de dniUsuario es: --> *,dniUsuario');
+
+//agui hariamos que el usuario ingrese su nombre
+prompt('Ingrese su nombre por favor!')
+
+nombreUsuario=prompt('Ingrese su apellido por favor!!');
+apellidoUsuario=prompt('Ingrese su apellido por favor!!');
+dniUsuario=prompt('Ingrese su numero de dni por favor!!');
+
+
+console.log('El valor de nombreUsuario es ---> ' + nombreusuario)
+console.log('El valor de apellidoUsuario es: -->' + apellidoUsuario);
+console.log('El valor de dniUsuario es: -->' + dniUsuario);
+
+document.write('El nombre de usuario ingresado es: ' + nombreusuario);
+document.write('<br/>El apellido de usuario ingresado es:' + apellidoUsuario);
+document.write('<br/>El dniUsuario de usuario ingresado es:' + dniUsuarioUsuario);
+*/
+/* let num_1;
+let num_2;
+
+num_1=prompt('Ingrese primer numero');
+num_2=prompt('Ingrese segundo numero');
+
+let num1_parse = parseInt(num_1);
+let num2_parse = parseInt(num_2);
+*/
+
+
+//resumiendo codigo
+let num_1;
+let num_2;
+
+num_1=parseInt(prompt('ingrese primer numero'));
+num_2=parseInt(prompt('Ingresar segundo numero'));
+
+
+let otraSuma = num_1 + num_2;
+
+document.write(otraSuma);
